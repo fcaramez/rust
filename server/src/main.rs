7d05e2b8c1e4291b598d90f06493c27a3b61579a
fcaramez <francisco.caramez.duarte@gmail.com>
2023-01-06 23:15:17 +0000

    completes exercise

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,12 @@
+use std::string;
+
 fn main() {
-    let server = Server::new("127.0.0.1:3000");
+    let string = String::from("127.0.0.1:3000");
+
+    let sliced_string = &string[10..];
+
+    let server = Server::new(string);
+
     server.run();
 }
 
